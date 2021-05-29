f14192dd67532e050b5e51c38ce02a6422410dbf
AliNijat <alinijat968@gmail.com>
2021-05-29 14:27:16 -0400

    Today's classes committed

---
+++ src/Day48_Constructor_Static/Group.java
@@ -0,0 +1,50 @@
+package Day48_Constructor_Static;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Group {
+
+   private String groupName;
+   private List<String> members;
+
+
+   public void addMember (String newMember){
+       members.add(newMember);
+   }
+
+   public void removeMember(String name){
+       members.remove(name);
+   }
+
+    public Group(String groupName){
+       this.groupName = groupName;
+        members = new ArrayList<>();
+    }
+
+    @Override
+    public String toString() {
+        return "Group{" +
+                "groupName='" + groupName + '\'' +
+                ", members=" + members +
+                '}';
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+
+    public List<String> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<String> members) {
+        this.members = members;
+    }
+
+
+}
---
+++ src/Day48_Constructor_Static/Group_18.java
@@ -0,0 +1,8 @@
+package Day48_Constructor_Static;
+
+public class Group_18 {
+    public static void main(String[] args) {
+
+
+    }
+}
