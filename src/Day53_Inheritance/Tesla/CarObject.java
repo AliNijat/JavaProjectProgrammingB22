0491edc16d7e1fa79559df041fd2647f4534b418
AliNijat <alinijat968@gmail.com>
2021-07-01 16:16:07 -0400

    first commit added

---
+++ src/Day53_Inheritance/Tesla/CarObject.java
@@ -0,0 +1,29 @@
+package Day53_Inheritance.Tesla;
+
+public class CarObject {
+
+    public static void main(String[] args) {
+
+        ElectricCar car1 = new ElectricCar("Tesla", "Medel Y", 28010.50, 2021, 326);
+        car1.drive(20);
+        System.out.println(car1);
+        System.out.println("Car1 make = " + car1.getMake());
+        System.out.println("Car1 model = " + car1.getModel());
+        System.out.println("Car1 price = $" + car1.getPrice());
+        System.out.println("Car1 year = " + car1.getYear());
+        System.out.println("Car1 range = " + car1.getRange());
+
+        if (car1.getPrice() > 30000){
+            System.out.println(car1.getMake() + " - " + car1.getModel() + " is out of my budget");
+        }else {
+            System.out.println("Purchasing " + car1);
+        }
+
+        System.out.println();
+
+        car1.setMake("BMW");
+        car1.setModel("SeriesC");
+        System.out.println(car1);
+
+    }
+}
